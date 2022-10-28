489df3d74620353513f083a248a57f7208f93d7f
Marcelo Mantovani <mantovani.marcelo98@outlook.com>
2022-10-28 18:00:40 -0300

    Test de funcionamento github

--- Calculadorapro/Program.cs
+++ Calculadorapro/Program.cs
@@ -10,7 +10,7 @@ namespace Calculadorapro
         public static decimal resultado = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Seja Bem-vindo à Calculadora!");
+            Console.WriteLine("Seja Bem-vindo b Calculadora!");
             bool tentarDnovo = true;
             while (tentarDnovo == true)
             {
